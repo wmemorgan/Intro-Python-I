597a2eacc259c97dea1d9d0f4d091ba37456719b
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-11-18 14:35:11 +0800

    Complete file_io.py

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+#VERSION 1:
+f = open('./src/foo.txt')
+read_data = f.read()
+print(read_data)
+f.close()
+
+# VERSION 2:
+with open('./src/foo.txt') as f:
+  read_data = f.read()
+  print(read_data)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +26,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+line = "All work and no play makes Jack a dull boy.\n"
+f = open('./src/bar.txt', 'w+')
+for i in range(3):
+  f.write(line)
+f.close()
+
+with open('./src/bar.txt', 'r') as f:
+  read_data = f.read()
+  print(read_data)
